c01f6c5abc5728e633bf07963b13854e63197135
Adriano Rosa <adrianorosa@sapo.pt>
2014-06-02 22:11:51 -0300

    improved I18n

--- lib/ReCaptcha/Captcha.php
+++ lib/ReCaptcha/Captcha.php
@@ -16,7 +16,7 @@ use ReCaptcha\CaptchaTheme;
 class Captcha
 {
    /**
-    * Constant API Server (defined without scheme)
+    * Constant API Server (without scheme)
     *
     * @var string
     **/
@@ -133,12 +133,9 @@ class Captcha
    /**
     * Create embedded widget script HTML called within form
     *
-    * @date 2014-05-30 22:44
-    * @author Adriano Rosa (http://adrianorosa.com)
-    *
-    * @param string $theme_name
-    * @param array $options = array()
-    * @return string The reCAPTCHA widget embedd HTML
+    * @param string $theme_name Optional Standard_Theme or custom theme name
+    * @param array $options Optional array of reCAPTCHA options
+    * @return string The reCAPTCHA widget embed HTML
     **/
    public function displayHTML($theme_name = NULL, $options = array())
    {
@@ -148,16 +145,12 @@ class Captcha
 
       $scheme = ( $this->_ssl ) ? 'https://' : 'http://';
       $errorpart = ($this->_errorResponse) ? $errorpart = "&amp;error=" . $this->_errorResponse : NULL;
-      // Overwrites config captcha_standard_theme value
-      if ( NULL !== $theme_name ) {
-         $this->_recaptchaOptions['theme'] = $theme_name;
-      }
 
-      $captcha_html = $this->_theme($options);
+      $captcha_html = $this->_theme($theme_name, $options);
       $captcha_html .= '<script type="text/javascript" src="'. $scheme . self::RECAPTCHA_API_SERVER . '/challenge?k=' . $this->_publicKey . $errorpart . '"></script>
 
       <noscript>
-         <iframe src="' . $scheme . self::RECAPTCHA_API_SERVER . '/noscript?k=' . $this->_publicKey . $errorpart . '" height="300" width="500" frameborder="0"></iframe><br>
+         <iframe src="' . $scheme . self::RECAPTCHA_API_SERVER . '/noscript?k=' . $this->_publicKey . $errorpart . '&hl=' . $this->userLang() . '" height="300" width="500" frameborder="0"></iframe><br>
          <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
          <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
       </noscript>';
@@ -169,17 +162,29 @@ class Captcha
    /**
     * Display Theme customization
     *
-    * @date 2014-05-30 23:22
-    * @author Adriano Rosa (http://adrianorosa.com)
-    *
+    * @param string $theme_name Optional overwrite's the config captcha_standard_theme value
     * @param array $options reCAPTCHA Options that can be overwridden for custom theme only
     * @return string Standard_Theme | Custom_Theme | Fallback default reCAPTCHA theme
     **/
-   private function _theme($options = array())
+   private function _theme($theme_name = NULL, $options = array())
    {
-      $options = array_merge($this->_recaptchaOptions, $options);
+      if ( NULL !== $theme_name ) {
+         $this->_recaptchaOptions['theme'] = $theme_name;
+      }
+
+      if ( count($options) > 0 ) {
+         $this->_recaptchaOptions = array_merge($this->_recaptchaOptions, $options);
+      }
+
+      if ( isset($this->_recaptchaOptions['lang'])
+            && !in_array($this->_recaptchaOptions['lang'], $this->_builtInlang)
+            && !isset($this->_recaptchaOptions['custom_translations']) ) {
+         $this->setTranslation($this->_recaptchaOptions['lang']);
+      }
+
+      $options = $this->_recaptchaOptions;
       unset($options['theme']); // We don't use theme=>value set in options array
-// _vd($options);
+
       // Skip to default reCAPTCHA theme if it's not set or options is not required
       if ( !isset($this->_recaptchaOptions['theme']) && count($options) == 0 ) {
          return;
@@ -242,7 +247,7 @@ class Captcha
    private function _customTheme($custom_options)
    {
       $js_options = '';
-// _vd($custom_options);
+
       foreach ($custom_options as $option => $value) {
 
          if ( is_array($value) ) {
@@ -265,9 +270,6 @@ class Captcha
       } else {
          $widget_ID = $custom_options['custom_theme_widget'];
       }
-      // $widget_ID = ( !isset($custom_options['custom_theme_widget']) )
-            // ? 'recaptcha_widget'
-            // : $custom_options['custom_theme_widget'];
 
       $custom_template = "
       <script type=\"text/javascript\">
@@ -283,23 +285,24 @@ class Captcha
             <div id="'. $widget_ID .'" style="display:none">
 
             <div id="recaptcha_image"></div>
-            <div class="recaptcha_only_if_incorrect_sol" style="color:red">{Incorrect, please try again.}'. $this->lang("incorrect_try_again") .'</div>
+            <div class="recaptcha_only_if_incorrect_sol" style="color:red">'. $this->i18n("incorrect_try_again") .'</div>
 
-            <span class="recaptcha_only_if_image">{Enter the words above:}'. $this->lang('instructions_visual') .'</span>
-            <span class="recaptcha_only_if_audio">{Enter the numbers you hear:}'. $this->lang('instructions_audio') .'</span>
+            <span class="recaptcha_only_if_image">'. $this->i18n('instructions_visual') .'</span>
+            <span class="recaptcha_only_if_audio">'. $this->i18n('instructions_audio') .'</span>
 
             <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />
 
-            <div><a href="javascript:Recaptcha.reload()">{Get another CAPTCHA}'. $this->lang('refresh_btn') .'</a></div>
-            <div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type(\'audio\')">{Get an audio CAPTCHA}'. $this->lang('audio_challenge') .'</a></div>
-            <div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type(\'image\')">{Get an image CAPTCHA}'. $this->lang('visual_challenge') .'</a></div>
+            <div><a href="javascript:Recaptcha.reload()">'. $this->i18n('refresh_btn') .'</a></div>
+            <div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type(\'audio\')">'. $this->i18n('audio_challenge') .'</a></div>
+            <div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type(\'image\')">'. $this->i18n('visual_challenge') .'</a></div>
 
-            <div><a href="javascript:Recaptcha.showhelp()">Help'. $this->lang('help_btn') .'</a></div>
+            <div><a href="javascript:Recaptcha.showhelp()">'. $this->i18n('help_btn') .'</a></div>
           </div>
           ';
        }
-       return vsprintf($custom_template, $this->_recaptchaOptions['custom_translations']);
+       return $custom_template;
    }
+
    /**
     * Custom Translations
     *
@@ -321,6 +324,38 @@ class Captcha
     **/
    public function setTranslation($language = 'en', $path = NULL)
    {
+      $this->_recaptchaOptions['lang'] = $language;
+
+      $custom_translations = $this->i18n(NULL, $path);
+
+      $this->_recaptchaOptions['custom_translations'] = $custom_translations;
+
+   }
+
+   /**
+    * Fetch I18n language line
+    *
+    * @date 2014-06-02 17:13
+    * @author Adriano Rosa (http://adrianorosa.com)
+    *
+    * @param string $key The string translated
+    * @param string $path Optional path to language file
+    * @return array|string
+    **/
+   protected function i18n($key = NULL, $path = NULL)
+   {
+      static $RECAPTCHA_LANG;
+
+      if ( $RECAPTCHA_LANG ) {
+         return isset($key) ? $RECAPTCHA_LANG[$key] : $RECAPTCHA_LANG;
+      }
+
+      if ( !isset($this->_recaptchaOptions['lang']) ) {
+         $language = 'en';
+      } else {
+         $language = $this->_recaptchaOptions['lang'];
+      }
+
       $RECAPTCHA_LANG = array(
          'instructions_visual' => 'Enter the words above:',
          'instructions_audio'  => 'Type what you hear:',
@@ -333,42 +368,33 @@ class Captcha
          'incorrect_try_again' => 'Incorrect, please try again.'
          );
 
-      // path/vendor/lib/I18n/recaptcha.lang.[langcode].php
+      // path/to/vendor/lib/I18n/recaptcha.lang.[langcode].php
       $path = ( NULL === $path )
          ? dirname(__DIR__) . DIRECTORY_SEPARATOR . 'I18n' . DIRECTORY_SEPARATOR
          : $path;
 
       if ( file_exists( $path . 'recaptcha.lang.' . $language . '.php' ) ) {
-         @include_once $path . 'recaptcha.lang.' . $language . '.php';
+         include_once $path . 'recaptcha.lang.' . $language . '.php';
       }
-      $this->_recaptchaOptions['custom_translations'] = $RECAPTCHA_LANG;
-      $this->_recaptchaOptions['lang'] = $language;
+
+      return isset($key) ? $RECAPTCHA_LANG[$key] : $RECAPTCHA_LANG;
+
    }
 
    /**
-    * Get the lang translations line
+    * Get user's browser language
     *
-    * @date 2014-06-02 02:50
-    * @author Adriano Rosa (http://adrianorosa.com)
-    *
-    * @param string $key
-    * @param string $lang
     * @return string
     **/
-   protected function lang($key, $lang = 'en')
+   public function userLang()
    {
-      if ( !isset($this->_recaptchaOptions['lang']) ) {
-         $this->setTranslation($lang);
-      } elseif ( isset($this->_recaptchaOptions['lang'])
-                  && !isset($this->_recaptchaOptions['custom_translations'])
-                  && !is_array($this->_recaptchaOptions['custom_translations'])
-                  ) {
-         $this->setTranslation($this->_recaptchaOptions['lang']);
+      if ( isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ) {
+
+         $language = explode(',', preg_replace('/(;\s?q=[0-9\.]+)|\s/i', '', strtolower(trim($_SERVER['HTTP_ACCEPT_LANGUAGE']))));
+         return strtolower($language[0]);
       }
 
-      return isset($this->_recaptchaOptions['custom_translations'][$key])
-         ? $this->_recaptchaOptions['custom_translations'][$key]
-         : false;
+      return;
    }
 
 }
